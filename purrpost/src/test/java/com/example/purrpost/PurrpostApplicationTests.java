1e813671bc2a651b9bd498c7c73f9b9cabe06966
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-03-08 23:31:34 +0800

    first commit

---
+++ purrpost/src/test/java/com/example/purrpost/PurrpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.purrpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurrpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
