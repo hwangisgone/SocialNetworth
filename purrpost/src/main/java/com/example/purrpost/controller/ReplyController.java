b791735e645109471aa8ac2ab7d63d8bedff79ac
quinntd2506 <trinhdiemquynh2506@gmail.com>
2024-03-27 10:49:35 +0700

    Update ReplyController.java

--- purrpost/src/main/java/com/example/purrpost/controller/ReplyController.java
+++ purrpost/src/main/java/com/example/purrpost/controller/ReplyController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -67,7 +69,16 @@ public class ReplyController {
 		}
 	}
 
-
+	@PostMapping("/reply")
+	public ResponseEntity<Reply> createReply(@RequestBody Reply reply) {
+		try {
+			Reply _reply = replyRepository.save(reply);
+			
+			return new ResponseEntity<>(_reply, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	
 }
