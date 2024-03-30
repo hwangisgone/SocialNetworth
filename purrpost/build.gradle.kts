07b864b6944f8b61f24ddebe86d7482070c57f69
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-03-30 16:24:47 +0800

    added security dependency

--- purrpost/build.gradle.kts
+++ purrpost/build.gradle.kts
@@ -18,8 +18,12 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	
 	// https://mvnrepository.com/artifact/org.postgresql/postgresql
 	implementation("org.postgresql:postgresql:42.7.1")
