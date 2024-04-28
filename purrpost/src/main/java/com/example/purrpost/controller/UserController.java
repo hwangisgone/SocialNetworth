09b22d9197f5fcd302d883a16ef16ed8ac330fba
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-04-28 23:35:40 +0800

    fix merging problems

--- purrpost/src/main/java/com/example/purrpost/controller/UserController.java
+++ purrpost/src/main/java/com/example/purrpost/controller/UserController.java
@@ -1,40 +1,23 @@
 package com.example.purrpost.controller;
 
-<<<<<<< HEAD
-import java.util.Date;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-=======
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
-import com.example.purrpost.model.Post;
-import com.example.purrpost.model.Tutorial;
-import com.example.purrpost.model.User;
-import com.example.purrpost.repository.UserRepository;
-=======
 import com.example.purrpost.model.User;
 import com.example.purrpost.repository.UserRepository;
 import com.example.purrpost.service.JwtTokenService;
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -53,34 +36,25 @@ public class UserController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-<<<<<<< HEAD
-	}	
-	@GetMapping("/user/login/{name_tag}/{password}")
-	public ResponseEntity<User> getTutorialByNameAndPassword(@PathVariable("name_tag") String name_tag, @PathVariable("password") String password){
-		List<User> foundUser = userRepository.findByNameTagAndPassword(name_tag, password);
-		
-		List<User> foundName = userRepository.findByNameTag(name_tag);
-		if( foundName.size() == 1) {
-			if (foundUser.size() == 1) {
-				 return new ResponseEntity<>(foundUser.get(0), HttpStatus.OK);
-			} else {
-				 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-			}	
-			
-		}
-		
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		
+
 	}
-	
-	 
-	@PostMapping("/users")
-	public ResponseEntity<User> createUser(@RequestBody User user) {
-		List<User> foundName = userRepository.findByNameTag(user.getNametag());
+
+	@PostMapping("/register")
+	public ResponseEntity<User> register(@RequestBody User user) {
+		List<User> foundName = userRepository.findByNameTag(user.getNameTag());
 		if (foundName.size() == 0) {
 			try {
-				User _user = userRepository
-						.save(new User(user.getUserId(), user.getNametag(), user.getPassword(), user.getName(), user.getEmail(), user.getBio(), user.getRole(), user.getPhone(), user.getRegistation_date()) );
+				User _user = userRepository.save(new User(
+					user.getUserId(),
+					user.getNameTag(),
+					user.getPassword(),
+					user.getName(),
+					user.getEmail(),
+					user.getBio(),
+					user.getRole(),
+					user.getPhone(),
+					user.getRegistrationDate())
+				);
 				return new ResponseEntity<>(_user, HttpStatus.CREATED);
 			} catch (Exception e) {
 				return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -88,10 +62,8 @@ public class UserController {
 			
 		}
 		return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-		
 	}
-=======
-	}
+	
 	
 	@Autowired
 	private JwtTokenService tokenService;
@@ -101,7 +73,6 @@ public class UserController {
 		List<User> foundUser = userRepository.findByNameTagAndPassword(user.getNameTag(), user.getPassword());
 
 		// Reference: https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/index.html#servlet-authentication-unpwd
-		
 		Authentication authRequest = UsernamePasswordAuthenticationToken.unauthenticated(user.getNameTag(), user.getPassword());
 		
 		HttpHeaders headers = new HttpHeaders();
@@ -113,8 +84,4 @@ public class UserController {
 			return new ResponseEntity<>(user, HttpStatus.UNAUTHORIZED);
 		}	
 	}
-	
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
-//	@GetMapping("/secured")
-	
 }
--- purrpost/src/main/java/com/example/purrpost/model/User.java
+++ purrpost/src/main/java/com/example/purrpost/model/User.java
@@ -1,9 +1,6 @@
 package com.example.purrpost.model;
 
-<<<<<<< HEAD
-=======
 import java.time.Instant;
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
 import java.util.Date;
 
 import jakarta.persistence.*; // for Spring Boot 3
@@ -40,27 +37,21 @@ public class User{
 	@Column(name = "phone")
 	private String phone;
 	
-<<<<<<< HEAD
-	@Column(name = "registation_date")
-	private Date registation_date;
-=======
 	@Column(name = "registration_date")
 	private Date registrationDate;
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
-	
+
 
 	public User() {
 		
 	}
+	
 	public User(String name_tag, String password) {
 		this.nameTag = name_tag; 
-<<<<<<< HEAD
-		this.password = password;	
-
+		this.password = password;
 	}
 	
 	public User(long userId, String nameTag, String password, String name, String email, String bio, String role,
-			String phone, Date registation_date) {
+			String phone, Date registrationDate) {
 		super();
 		this.userId = userId;
 		this.nameTag = nameTag;
@@ -70,104 +61,80 @@ public class User{
 		this.bio = bio;
 		this.role = role;
 		this.phone = phone;
-		this.registation_date = registation_date;
-	}
-	public String getNametag() {
-		return nameTag;
-	}
-	
-	public String getPassword() {
-		return password;
-		
-	}
-	public long getUserId() {
-		return userId;
-	}
-	public void setUserId(long userId) {
-		this.userId = userId;
-	}
-=======
-		this.password = password;
+		this.registrationDate = registrationDate;
 	}
 
 	public void setDefaultUser() {
 		this.registrationDate = Date.from(Instant.now());
 		this.role = "user";
 	}
-	
-	
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
+
 	public String getNameTag() {
 		return nameTag;
 	}
+
 	public void setNameTag(String nameTag) {
 		this.nameTag = nameTag;
 	}
-<<<<<<< HEAD
-=======
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getBio() {
 		return bio;
 	}
+
 	public void setBio(String bio) {
 		this.bio = bio;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-<<<<<<< HEAD
-	public Date getRegistation_date() {
-		return registation_date;
-	}
-	public void setRegistation_date(Date registation_date) {
-		this.registation_date = registation_date;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
-	
-
-
-	
-	
-
-=======
 	public Date getRegistrationDate() {
 		return registrationDate;
 	}
+
 	public void setRegistrationDate(Date registrationDate) {
 		this.registrationDate = registrationDate;
 	}
+
 	public long getUserId() {
 		return userId;
 	}
->>>>>>> 3bff5ee8c13e2728af7abbb779562b3bb7af8259
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
