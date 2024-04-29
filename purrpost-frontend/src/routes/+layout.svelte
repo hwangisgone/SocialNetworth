b3f33c65f73428a1f55bdfe65c66ffc035dab64a
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2024-04-29 09:54:44 +0800

    feat: moved from contributions to svelte

--- purrpost-frontend/src/routes/+layout.svelte
+++ purrpost-frontend/src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-  import "../app.css";
+	import "../app.css";
+
+	import Detached from './components/Detached.svelte';
 </script>
+
+<Detached>
+	<div class="p-4 flex flex-col gap-2 bg-black rounded-xl ">
+		<span class="font-bold text-center">Temp test</span>
+		<a href="./login" class="btn btn-primary btn-sm">Go to Login</a>
+		<a href="./register" class="btn btn-primary btn-sm">Go to Register</a>
+		<a href="./" class="btn btn-primary btn-sm">Go to Home</a>
+	</div>
+</Detached>
 
 <slot />
--- purrpost-frontend/src/routes/+page.svelte
+++ purrpost-frontend/src/routes/+page.svelte
@@ -1,98 +1,126 @@
-  <div class="flex min-h-screen items-center justify-center rounded-3xl">
-		<div class="nd-background w-2/3 min-h-1/2 border border-gray-900 rounded-3xl">
+<script lang='ts'>
+	import EyeIcon		from '$lib/icon/eye.svg?component';
+	import HeartIcon	from '$lib/icon/heart.svg?component';
+	import ReplyIcon	from '$lib/icon/talk-bubbles.svg?component';
+	import ShareIcon	from '$lib/icon/share.svg?component';
 
-			<div class="mx-4 sm:mx-24 md:mx-34 lg:mx-56 mx-auto  flex items-center space-y-4 py-16 font-semibold text-gray-500 flex-col">
-				<svg viewBox="0 0 24 24" class=" h-12 w-12 text-white" fill="currentColor">
-					<g>
-						<path
-							d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z">
-						</path>
-					</g>
-				</svg>
+	import PurrPostLogo from '$lib/purrpost-logo.svg?component'; 
 
-				<h1 class="text-white text-2xl">Join PurrPost today</h1>
-				<!-- <button class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 "> <img src="your-image-url.jpg" alt="Apple Logo" class="inline-block h-6 w-6 mr-2"> Sign up with Google</button> -->
-				<button class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 flex items-center justify-center">
-				  <svg width="40" height="40" viewBox="0 0 44 45" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
-					  <g clip-path="url(#clip0_62_1037)">
-						  <path d="M8.99591 17.0888C9.79521 14.6115 11.3378 12.4562 13.4036 10.9301C15.4694 9.40412 17.9529 8.58539 20.4998 8.59075C23.3869 8.59075 25.9972 9.64075 28.0472 11.3593L34.0127 5.25C30.3774 2.00375 25.7188 0 20.4998 0C12.4194 0 5.46308 4.7215 2.11816 11.6375L8.99591 17.0888Z" fill="#EA4335"/>
-						  <path d="M27.4017 31.5227C25.5397 32.753 23.1753 33.4092 20.5001 33.4092C17.9633 33.4146 15.4892 32.6024 13.428 31.0876C11.3668 29.5729 9.82289 27.4323 9.01495 24.969L2.11328 30.3362C3.80666 33.8473 6.42728 36.8014 9.67748 38.8632C12.9277 40.925 16.6772 42.0117 20.5001 42C25.5106 42 30.2974 40.1747 33.8832 36.75L27.4035 31.5227H27.4017Z" fill="#34A853"/>
-						  <path d="M33.8831 36.75C37.6329 33.166 40.0673 27.832 40.0673 21C40.0673 19.7575 39.881 18.4222 39.6026 17.1815H20.5V25.2962H31.4948C30.9533 28.0245 29.4961 30.1367 27.4034 31.5227L33.8831 36.75Z" fill="#4A90E2"/>
-						  <path d="M9.01507 24.969C8.59714 23.6896 8.38482 22.3492 8.38641 21C8.38641 19.6315 8.59995 18.3173 8.99628 17.0888L2.11853 11.6375C0.711511 14.5472 -0.0138032 17.7529 0.000198943 21C0.000198943 24.36 0.760407 27.5275 2.11341 30.3363L9.01507 24.969Z" fill="#FBBC05"/>
-					  </g>
-					  <defs>
-						  <clipPath id="clip0_62_1037">
-							  <rect width="41" height="42" fill="white"/>
-						  </clipPath>
-					  </defs>
-				  </svg>
-				  Sign up with Google
-			  </button>
-			  
-			  <button class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 flex items-center justify-center">
-				  <svg width="40" height="40" viewBox="0 0 44 45" fill="none" xmlns="http://www.w3.org/2000/svg" class="mr-2">
-					  <g clip-path="url(#clip0_62_1034)">
-						  <path d="M18.1875 42.1542C8.80208 40.4979 1.625 32.3271 1.625 22.5C1.625 11.5688 10.5688 2.625 21.5 2.625C32.4312 2.625 41.375 11.5688 41.375 22.5C41.375 32.3271 34.1979 40.4979 24.8125 42.1542L23.7083 41.2708H19.2917L18.1875 42.1542Z" fill="url(#paint0_linear_62_1034)"/>
-						  <path d="M29.2292 28.0208L30.1125 22.5H24.8125V18.6354C24.8125 17.0896 25.3646 15.875 27.7938 15.875H30.3333V10.7958C28.8979 10.575 27.3521 10.3542 25.9167 10.3542C21.3896 10.3542 18.1875 13.1146 18.1875 18.0833V22.5H13.2188V28.0208H18.1875V42.0437C19.2917 42.2646 20.3958 42.375 21.5 42.375C22.6042 42.375 23.7083 42.2646 24.8125 42.0437V28.0208H29.2292Z" fill="white"/>
-					  </g>
-					  <defs>
-						  <linearGradient id="paint0_linear_62_1034" x1="21.5" y1="40.9992" x2="21.5" y2="2.625" gradientUnits="userSpaceOnUse">
-							  <stop stop-color="#0062E0"/>
-							  <stop offset="1" stop-color="#19AFFF"/>
-						  </linearGradient>
-						  <clipPath id="clip0_62_1034">   
-							  <rect width="44" height="45" fill="white"/>
-						  </clipPath>
-					  </defs>
-				  </svg>
-				  Sign up with Facebook
-			  </button>
-			  
-			  
-				<div class="w-full mx-auto max-w-sm transform space-y-4 text-center">
-				  <div class="w-full flex items-center space-x-4">
-					  <hr class="w-full border-b" />
-					  <div class="text-sm text-gray-400">Or</div>
-					  <hr class="w-full border-b" />
-				  </div>
-				<p class="text-white text-xs pt-6 pb-16">Enter your email and password</p>
-				<div
-				  class="w-full transform border-b bg-transparent duration-300 text-sm focus-within:border-indigo-500"
-				>
-				  <input
-					type="text"
-					placeholder="Email"
-					class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none text-white"
-				  />
+	const postArray = [
+		{
+			pics: ['https://i.imgur.com/fjXFX93.jpeg'],
+			viewCount: 10,
+			likeCount: 3,
+			replyCount: 1,
+			shareCount: 0,
+		},
+		{
+			pics: ['https://i.imgur.com/5yeBVeM.jpeg'],
+			viewCount: 1,
+			likeCount: 1,
+			replyCount: 0,
+			shareCount: 0,
+		},
+		{
+			pics: ['https://i.imgur.com/DBpznrn.jpeg'],
+			viewCount: 100,
+			likeCount: 27,
+			replyCount: 15,
+			shareCount: 7,
+		},
+		{
+			pics: ['https://i.imgur.com/ISpNf4j.jpeg'],
+			viewCount: 20,
+			likeCount: 20,
+			replyCount: 19,
+			shareCount: 0,
+		}
+	];
+
+	import SearchBar from './SearchBar.svelte';
+	import PostPopup from './PostPopup.svelte';
+</script>
+
+<div class="flex min-h-screen w-screen items-center justify-center rounded-3xl">
+	<div class="nd-background w-screen min-h-1/2 border border-gray-900 rounded-3xl">
+				<!-- component -->
+		<div class="h-screen w-full relative flex overflow-hidden">
+
+			<!-- Sidebar -->
+			<aside class="h-full w-2/12 flex flex-col space-y-10 items-center relative bg-gray-800 text-white px-4">
+				<!-- Logo -->
+				<div class="relative flex-shrink-0 px-2 py-2 w-full text-gray-400 border-b dark:border-indigo-800 dark:focus-within:text-light focus-within:text-gray-700 text-center">
+					<a class="btn btn-ghost btn-circle" href="./"> 
+						<PurrPostLogo width=30 height=30/>
+					</a>
 				</div>
 
-				<div
-				  class="w-full transform border-b bg-transparent duration-300 text-sm focus-within:border-indigo-500"
-				>
-				  <input
-					type="password"
-					placeholder="Password"
-					class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-				  />
-				</div>
-				<div
-				  class="w-full transform border-b bg-transparent duration-300 text-sm focus-within:border-indigo-500"
-				>
-				  <input
-					type="password"
-					placeholder="Confirm Password"
-					class="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
-				  />
-				</div>
-				
-				<button class="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 "> Create account</button>
+				<!-- Empty space -->
+				<div class="pb-24"></div>
 
+				<PostPopup />
+			</aside>
 
+			
+		 
+			<div class="w-7/12 h-full flex flex-col justify-between">
+				<!-- Header -->
+				<header class="h-16 w-full flex items-center relative justify-end px-5 space-x-10 bg-gray-800">
+				<!-- Profile -->
+					<div class="relative flex-shrink-0 px-2 py-2 w-full text-gray-400 dark:focus-within:text-light focus-within:text-gray-700">
+							<button class="w-full p-2 bg-gray-50 rounded-full bg-opacity-0 font-bold text-white"> <a class="border-b dark:border-indigo-800">For you</a></button>
+					</div>
+				</header>
 
-					<p class="text-sm">Already have an account? 
-						<a class="font-semibold text-sky-700" href="">Sign in</a> </p>
-				
-				</div>
+				<!-- Main -->
+				<main class="max-w-full h-full flex relative overflow-y-hidden">
+					<!-- Container -->
+					<div id="imageContainer" class="h-full w-full flex flex-wrap items-start bg-opacity-0 justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
+						<!-- Container -->
+						<!-- For each in svelte -->
+						{#each postArray as post}
+							<div class="w-full bg-opacity-0 p-10">
+								<img class="w-full object-cover" src={post.pics[0]}>
+								<ul class="mt-3 flex flex-wrap">
+									<li class="mr-auto">
+										<a href="#" class="flex text-gray-400 hover:text-gray-600">
+											<ShareIcon width=24 height=24/>
+											{post.shareCount}
+										</a>
+									</li>
+									<li class="mr-2">
+										<a href="#" class="flex text-gray-400 hover:text-gray-600">
+											<EyeIcon width=24 height=24/>
+											{post.viewCount}
+										</a>
+									</li>
+									<li class="mr-2">
+										<a href="#" class="flex text-gray-400 hover:text-gray-600">
+											<ReplyIcon width=24 height=24/>
+											{post.replyCount}
+										</a>
+									</li>
+									<li>
+										<a href="#" class="flex text-gray-400 hover:text-gray-600">
+											<HeartIcon width=24 height=24/>
+											{post.likeCount}
+										</a>
+									</li>
+								</ul>
+							</div>
+						{/each}
+						<!-- For each in svelte -->
+					</div>
+				</main>
+
 			</div>
+
+			<!-- Search Bar -->
+			<aside class="h-full w-3/12 flex flex-col space-y-10 items-center relative bg-gray-800 text-white">
+				<SearchBar />
+			</aside>
 		</div>
+
 	</div>
+
+</div>
