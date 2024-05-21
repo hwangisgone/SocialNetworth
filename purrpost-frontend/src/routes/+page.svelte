9d09051fea6a73830832b82edba866cc703d8c16
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-05-21 12:40:16 +0700

    home main body

--- purrpost-frontend/src/routes/+page.svelte
+++ purrpost-frontend/src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import HeartIcon	from '$lib/icon/heart.svg?component';
 	import ReplyIcon	from '$lib/icon/talk-bubbles.svg?component';
 	import ShareIcon	from '$lib/icon/share.svg?component';
+	import ContentBody  from './ContentBody.svelte';
 
 	import PurrPostLogo from '$lib/purrpost-logo.svg?component'; 
 
@@ -93,7 +94,7 @@
 				  <!-- Container -->
 				   <div id="imageContainer" class="w-full m-4 flex flex-wrap items-start bg-opacity-0 justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
 					<!-- Container -->
-						  
+					<ContentBody />	  
 					</div>
 				</main>
 	  
