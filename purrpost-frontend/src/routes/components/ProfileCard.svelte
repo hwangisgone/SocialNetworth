a1f99cf92e1495b47114bbd5b5ff87fbbb9e138e
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-06-10 16:11:01 +0700

    Components

---
+++ purrpost-frontend/src/routes/components/ProfileCard.svelte
@@ -0,0 +1,102 @@
+<!--Profile card start-->
+<div class="card bg-opacity-1 w-full overflow-hidden  
+rounded-lg shadow-lg flex flex-col"> 
+  <div class="card-image"> 
+    <img src="https://i.imgur.com/fjXFX93.jpeg"
+    alt="Image" 
+    class="w-full h-48 object-cover rounded-t-lg" /> 
+  </div> 
+  <div class="profile-image flex"> 
+    <img src= 
+    "https://i.imgur.com/GCBVgXD.jpeg" 
+    alt=""
+    class="float-left z-10 w-24 h-24 relative ml-16 -mt-16  
+    block rounded-full border-2 border-gary-800  
+    transition-transform duration-400  
+    transform hover:scale-110" /> 
+    <div class="flex-grow"></div>
+    <div class="flex float-right items-end text-base leading-6 font-medium text-gray-800 dark:text-white">
+      <div class="pr-20">
+        <div class="flex items-center h-full text-gray-800 dark:text-white">
+          <a href="#" class="text-xs font-bold text-blue-400 px-4 py-1 rounded-full border-2 border-blue-400">Follow</a>
+        </div>
+      </div>
+    </div>
+  </div> 
+  <div class="card-content py-2"> 
+    <div class="pl-16">
+      <div class="">
+        <p class="flex items-center text-base leading-6 font-medium text-gray-800 dark:text-white">
+          Profile Name
+        </p>
+        <span class="ml-1 text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
+          @Username
+        </span>
+      </div>
+      <p class="text-base width-auto font-medium text-gray-800 dark:text-white flex-shrink">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      </p>
+      <div class="flex">
+        <div class="w-full">
+          <div class="flex items-center">
+            <div class="flex-1 flex items-center text-gray-300 dark:text-white-500 hover:text-green-400 dark:hover:text-green-400 transition duration-350">
+              <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M6.95368 0C10.7873 0 13.9074 3.12004 13.9074 6.95208V6.95369C13.9074 8.4761 13.4258 9.91932 12.5138 11.1321L12.5122 11.1305L12.5066 11.1417C11.2041 12.7994 7.48409 15.7474 7.32649 15.8714C7.21609 15.9586 7.08489 16.001 6.95449 16.001C6.82408 16.001 6.69288 15.9586 6.58248 15.8706C6.42488 15.7466 2.70483 12.797 1.40242 11.1409L1.39682 11.1289L1.39442 11.1305C0.482406 9.91852 0 8.4737 0 6.95208C0 3.12004 3.12004 0 6.95368 0ZM2.34883 10.4041C3.33764 11.6601 5.97447 13.8402 6.95368 14.6322V14.6306C7.9329 13.8378 10.5697 11.6585 11.5585 10.4025C12.3097 9.40172 12.7074 8.2081 12.7074 6.95208C12.7074 3.78085 10.1257 1.20001 6.95368 1.20001C3.78165 1.20001 1.20001 3.78165 1.20001 6.95448C1.20001 8.2105 1.59682 9.40411 2.34883 10.4041ZM3.92388 6.82328C3.92388 8.4937 5.28309 9.85211 6.95351 9.85211V9.84971C8.62313 9.84971 9.98315 8.4913 9.98315 6.82168C9.98315 5.15206 8.62313 3.79204 6.95351 3.79204C5.28389 3.79204 3.92388 5.15286 3.92388 6.82328ZM5.12389 6.82328C5.12389 5.81446 5.9455 4.99365 6.95351 4.99365V4.99205C7.96152 4.99205 8.78314 5.81366 8.78314 6.82248C8.78314 7.83129 7.96152 8.6513 6.95351 8.6513C5.9455 8.6513 5.12389 7.83209 5.12389 6.82328Z" fill="currentColor"></path>
+              </svg>
+
+              City, Country
+            </div>
+            <div class="flex-1 flex items-center text-gray-300 dark:text-white-500 hover:text-red-600 dark:hover:text-red-600 transition duration-350">
+              <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.81859 11.3017C7.87238 11.3154 7.92777 11.3234 7.98156 11.3234L7.97995 11.325C8.24327 11.325 8.48572 11.1508 8.55957 10.8859C8.65029 10.5647 8.46324 10.2324 8.14212 10.1409C7.49587 9.95864 6.94274 9.57731 6.54295 9.03542C6.06689 8.39078 5.8686 7.60003 5.98902 6.80526C6.10864 6.0137 6.5293 5.31447 7.17315 4.83841L10.007 2.74472C11.338 1.76049 13.2222 2.04468 14.2056 3.37411C14.6809 4.01876 14.8784 4.81111 14.7596 5.60428C14.6399 6.39664 14.2193 7.09587 13.5746 7.57113L12.3865 8.45019C12.1184 8.64767 12.0622 9.02499 12.2597 9.29312C12.4571 9.56045 12.8353 9.61745 13.1026 9.41996L14.2907 8.5417C15.1955 7.87298 15.7855 6.89357 15.9525 5.78169C16.1195 4.66982 15.8425 3.55956 15.1762 2.65481C13.7954 0.78911 11.1558 0.394135 9.29172 1.77173L6.45785 3.86542C5.55391 4.53415 4.96466 5.51356 4.79767 6.62543C4.63069 7.7373 4.90685 8.84757 5.57478 9.75071C6.13513 10.5102 6.91143 11.0464 7.81859 11.3017ZM0.824646 15.3245C1.64992 16.4404 2.92396 17.0305 4.21646 17.0305L4.21565 17.0288C5.08428 17.0288 5.95932 16.7615 6.70833 16.2052L9.5422 14.1115C11.4079 12.7323 11.8037 10.0927 10.4253 8.227C9.86492 7.46755 9.08942 6.93129 8.18227 6.676C7.86275 6.5893 7.52879 6.77394 7.43888 7.09506C7.34816 7.41618 7.53521 7.74853 7.85633 7.83925C8.50258 8.02068 9.05571 8.40281 9.4555 8.94389C10.4389 10.2765 10.1563 12.1591 8.8253 13.1425L5.99304 15.2362C4.6604 16.2196 2.77785 15.9362 1.79442 14.606C1.31836 13.9614 1.12168 13.1706 1.24049 12.3758C1.36011 11.5843 1.77997 10.8851 2.42462 10.409L3.61275 9.52994C3.88089 9.33245 3.93548 8.95433 3.73879 8.687C3.54211 8.41967 3.16399 8.36347 2.89586 8.56016L1.70933 9.43842C0.804576 10.1071 0.214521 11.0874 0.0475398 12.1984C-0.119442 13.3095 0.15672 14.4198 0.824646 15.3245Z" fill="currentColor"></path>
+              </svg>
+
+              @website
+            </div>
+            <div class="flex-1 flex items-center text-gray-300 dark:text-white-500 hover:text-blue-400 dark:hover:text-blue-400 transition duration-350">
+              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M14.1664 0H1.8336C0.8224 0 0 0.8224 0 1.8336V14.1664C0 15.1776 0.8224 16 1.8336 16H14.1664C15.1776 16 16 15.1776 16 14.1664V1.8336C16 0.8224 15.1776 0 14.1664 0ZM14.8 14.1664C14.8 14.516 14.516 14.8 14.1664 14.8H1.8336C1.484 14.8 1.2 14.516 1.2 14.1664V3.5344C1.2 3.1848 1.4832 2.9024 1.832 2.9008H14.168C14.5168 2.9008 14.8 3.1848 14.8 3.5328V14.1664ZM4.02566 6.428C4.5934 6.428 5.05366 5.96775 5.05366 5.4C5.05366 4.83225 4.5934 4.372 4.02566 4.372C3.45791 4.372 2.99766 4.83225 2.99766 5.4C2.99766 5.96775 3.45791 6.428 4.02566 6.428ZM5.05366 8.9248C5.05366 9.49255 4.5934 9.9528 4.02566 9.9528C3.45791 9.9528 2.99766 9.49255 2.99766 8.9248C2.99766 8.35705 3.45791 7.8968 4.02566 7.8968C4.5934 7.8968 5.05366 8.35705 5.05366 8.9248ZM11.9745 6.428C12.5422 6.428 13.0025 5.96775 13.0025 5.4C13.0025 4.83225 12.5422 4.372 11.9745 4.372C11.4067 4.372 10.9465 4.83225 10.9465 5.4C10.9465 5.96775 11.4067 6.428 11.9745 6.428ZM13.0025 8.9248C13.0025 9.49255 12.5422 9.9528 11.9745 9.9528C11.4067 9.9528 10.9465 9.49255 10.9465 8.9248C10.9465 8.35705 11.4067 7.8968 11.9745 7.8968C12.5422 7.8968 13.0025 8.35705 13.0025 8.9248ZM7.99988 6.428C8.56762 6.428 9.02787 5.96775 9.02787 5.4C9.02787 4.83225 8.56762 4.372 7.99988 4.372C7.43213 4.372 6.97188 4.83225 6.97188 5.4C6.97188 5.96775 7.43213 6.428 7.99988 6.428ZM9.02787 8.9248C9.02787 9.49255 8.56762 9.9528 7.99988 9.9528C7.43213 9.9528 6.97188 9.49255 6.97188 8.9248C6.97188 8.35705 7.43213 7.8968 7.99988 7.8968C8.56762 7.8968 9.02787 8.35705 9.02787 8.9248ZM4.02566 13.4168C4.5934 13.4168 5.05366 12.9565 5.05366 12.3888C5.05366 11.821 4.5934 11.3608 4.02566 11.3608C3.45791 11.3608 2.99766 11.821 2.99766 12.3888C2.99766 12.9565 3.45791 13.4168 4.02566 13.4168ZM9.02787 12.3888C9.02787 12.9565 8.56762 13.4168 7.99988 13.4168C7.43213 13.4168 6.97188 12.9565 6.97188 12.3888C6.97188 11.821 7.43213 11.3608 7.99988 11.3608C8.56762 11.3608 9.02787 11.821 9.02787 12.3888Z" fill="currentColor"></path>
+              </svg>
+
+              Joined ...
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex">
+        <div class="">
+          <div class="flex items-center">
+            <div class="flex-1 flex items-center text-gray-800 text-sm dark:text-gray-400 hover:text-blue-400 dark:hover:text-blue-400 transition duration-350 ease-in-out pr-20">
+              <p class="text-black dark:text-white text-base">123</p>
+              Following
+            </div>
+            <div class="flex-1 flex items-center text-gray-800 text-sm dark:text-gray-400 hover:text-green-400 dark:hover:text-green-400 transition duration-350 ease-in-out">
+              <p class="text-black dark:text-white text-base">456</p>
+              Followers
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex pt-5">
+        <div class="w-full">
+          <div class="flex items-center">
+            <div class="flex-1 flex items-center text-gray-800 dark:text-white text-gray-400 hover:text-blue-400 dark:hover:text-blue-400 transition duration-350 ease-in-out">
+              Posts
+            </div>
+            <div class="flex-1 flex items-center text-gray-800 dark:text-white text-gray-400 hover:text-green-400 dark:hover:text-green-400 transition duration-350 ease-in-out">
+              Posts &amp; Replies
+            </div>
+            <div class="flex-1 flex items-center text-gray-800 dark:text-white text-gray-400 hover:text-red-600 dark:hover:text-red-600 transition duration-350 ease-in-out">
+              Media
+            </div>
+            <div class="flex-1 flex items-center text-gray-800 dark:text-white text-gray-400 hover:text-blue-400 dark:hover:text-blue-400 transition duration-350 ease-in-out">
+              Likes
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> 
+</div>  
+<!--Profile card end-->
