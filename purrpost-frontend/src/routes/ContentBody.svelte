c6cb54ebf9967715ba7a721276b1122f23efc794
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-06-10 16:46:51 +0700

    Comment

--- purrpost-frontend/src/routes/ContentBody.svelte
+++ purrpost-frontend/src/routes/ContentBody.svelte
@@ -1,9 +1,11 @@
 <script lang='ts'>
 	import Post		from './components/Post.svelte';
+	import CommentContainer		from './components/CommentContainer.svelte';
 	import EditProfile	from './components/EditProfileCard.svelte';
 	import ProfileCard		from './components/ProfileCard.svelte';
 </script>
 
 <Post />
+<CommentContainer />
 <ProfileCard />
 <EditProfile />
